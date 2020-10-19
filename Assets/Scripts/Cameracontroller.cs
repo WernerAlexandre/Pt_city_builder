48151b40e8ec507def51cc7ffd30fe358e902bca
WernerAlexandre <alexandre.werner4@etu.univ-lorraine.fr>
2020-10-19 11:21:58 +0200

    first commit

---
+++ Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Cameracontroller : MonoBehaviour
+{
+    public Transform cameraTransform; //Pour pouvoir manipuler les positions de la caméra
+
+    public float normalSpeed;
+    public float fastSpeed;
+    public float movementSpeed;
+    public float movementTime;
+    public float rotationAmount;
+    public float zoomSpeed;
+    public float maxZoomDist;
+    public float minZoomDist;
+    
+    public Vector3 newPosition;
+    public Vector3 zoomAmount;
+    public Vector3 newZoom;
+    public Vector3 dragStartPos;
+    public Vector3 dragCurrentPos;
+    public Vector3 rotateStartPos;
+    public Vector3 rotateCurrentPos;
+
+    public Quaternion newRotation;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position; // replacement
+        newRotation = transform.rotation; // place la rotation
+        newZoom = cameraTransform.localPosition; // place le zoom de début sur la bonne position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+        HandleMouseInput();
+    }
+
+    private Camera cam; // création d'une camera
+
+    private void Awake()
+    {
+        cam = Camera.main; // attribut la première camera étiquetée "MainCamera" à cam
+    }
+
+
+    
+
+    // Méthode de gestion de la souris
+    void HandleMouseInput()
+    {
+        // le zoom avec le mouseScroll
+        if(Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount * 8; // On multiplie par 8 car le zoomAmount clavier/souris n'a pas autant de puissance sur l'un que sur l'autre
+        }
+
+        // déplacement au clic gauche
+        if (Input.GetMouseButtonDown(0)) // Retourne true pendant que l'utilisateur presse le bouton donné, ici 0 (clique gauche)
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float entry;
+
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPos = ray.GetPoint(entry);
+            }
+        }
+
+        if (Input.GetMouseButton(0)) // retourne true si le bouton donné est maintenu enfoncé
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero); // création d'un plan avec deux vecteurs 0,1,0 et 0,0,0
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Renvoie un rayon allant de la caméra à un point de l'écran, ici la position de la souris
+            float entry;
+
+            if (plane.Raycast(ray, out entry)) // vérifie si le rayon et le plan se croisent, si le rayon esst parallèle au plan, entry = 0 et on renvoie false
+            {
+                dragCurrentPos = ray.GetPoint(entry);
+                newPosition = transform.position + dragStartPos - dragCurrentPos;
+            }
+        }
+
+        // rotation avec le clic du milieu
+        if (Input.GetMouseButtonDown(2)) // meme fonctionnement qu'au dessus, le 2 correspond au clic du milieu
+        {
+            rotateStartPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            rotateCurrentPos = Input.mousePosition; // on récupère la position de la souris
+            Vector3 difference = rotateStartPos - rotateCurrentPos;
+            rotateStartPos = rotateCurrentPos; // actualisation
+            newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f)); // la valeur est négative pour plus d'ergonomie en jeu
+        }
+
+    }
+
+    // Méthode de gestion du clavier
+    void HandleMovementInput()
+    {
+        // Sprint de camera
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = fastSpeed;
+        }
+        else
+        {
+            movementSpeed = normalSpeed;
+        }
+
+        // ZQSD CONTROLLER
+        if(Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -movementSpeed);
+        }
+
+        // ROTATION CONTROLLER
+        if (Input.GetKey(KeyCode.A))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        // ZOOM CONTROLLER
+        if (Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            newZoom -= zoomAmount;
+        }
+
+
+        // SMOOTH MOVEMENT
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+    }
+
+}
---
+++ Assets/Scripts/SpawnBuildings.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class SpawnBuildings : MonoBehaviour
+{
+    #region Inspector Variables
+    [TooltipAttribute("The tile GameObject that make up the grid")]
+    [SerializeField] GameObject productionTile;
+
+    [TooltipAttribute("The layer in which the terrain is placed")]
+    [SerializeField] LayerMask terrainLayer;
+
+    [TooltipAttribute("Need GraphicRaycaster to detect click on a button")]
+    [SerializeField] GraphicRaycaster uiRaycaster;
+
+    [SerializeField] GameObject underConstructionGO;
+    [SerializeField] BuildProgressSO buildingToPlace;
+    #endregion
+
+    #region Instance Objects
+    GameObject currentSpawnedBuilding;
+    RaycastHit hit;
+    List<ProductionTile> activeTiles;
+    GameObject activeTilesParent;
+    #endregion
+
+    Vector3 truePos;
+    public float gridSize;
+
+    void Start ()
+    {
+        activeTiles = new List<ProductionTile>();
+        if (!productionTile)
+            Debug.LogError("Production Tile is NULL");
+        if (!uiRaycaster)
+            Debug.Log("GraphicRaycaster not found! Will place objects on button click");
+	}
+
+
+    void Update()
+    {
+        if (currentSpawnedBuilding)
+        {
+            // Si clic gauche
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!PlacementHelpers.RaycastFromMouse(out hit, terrainLayer))
+                    return;
+                float gridDecal = 0.5f;
+                // Déplacer la grille de construction suivant l'axe X 
+                truePos.x = Mathf.Ceil(hit.point.x / gridSize) * gridSize - gridDecal;
+                // Garder la grille de construction constante sur l'axe Y
+                truePos.y = currentSpawnedBuilding.gameObject.transform.localScale.y/2;
+                // Déplacer la grille de construction suivant l'axe Z
+                truePos.z = Mathf.Ceil(hit.point.z / gridSize) * gridSize - gridDecal;
+
+                // Donne la position réel de l'objet à construire par rapport à la souris
+                currentSpawnedBuilding.transform.position = truePos;
+
+                // Si il n'y a pas de collision lancer la construction de l'objet
+                if(CanPlaceBuilding())
+                    PlaceBuilding();
+            }
+            // Si clic droit annuler la prévisualisation de la zone de construction
+            if (Input.GetMouseButtonDown(1))
+                Destroy(currentSpawnedBuilding);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if(currentSpawnedBuilding)
+            if(PlacementHelpers.RaycastFromMouse(out hit, terrainLayer)){
+                // Positionne l'objet suivant l'axe X 
+                truePos.x = Mathf.Ceil(hit.point.x / gridSize) * gridSize;
+                // Garder l'objet constante sur l'axe Y
+                truePos.y = 0;
+                // Positionne l'objet suivant l'axe Z
+                truePos.z = Mathf.Ceil(hit.point.z / gridSize) * gridSize;
+
+                // Donne la position réel de l'objet à construire
+                currentSpawnedBuilding.transform.position = truePos;
+            }
+    }
+
+
+    bool CanPlaceBuilding()
+    {
+        if (PlacementHelpers.IsButtonPressed(uiRaycaster))
+            return false;
+        for(int i = 0; i < activeTiles.Count; i++)
+            if(activeTiles[i].colliding)
+                return false;
+        return true;
+    }
+
+
+    void PlaceBuilding()
+    {
+        ClearGrid();
+        StartCoroutine(BeginBuilding());
+    }
+
+
+    void ClearGrid()
+    {
+        Destroy(activeTilesParent);
+        activeTiles.RemoveAll(i => i);
+    }
+
+
+    IEnumerator BeginBuilding()
+    {
+        Vector3 pos = truePos;
+        GameObject instance = currentSpawnedBuilding;
+        currentSpawnedBuilding = null;
+        Debug.Log(""+ truePos);
+
+        RaycastHit hitTerrain;
+        if (PlacementHelpers.RaycastFromMouse(out hitTerrain, terrainLayer))
+            pos = hitTerrain.point;
+
+        GameObject go = Instantiate(underConstructionGO, truePos, Quaternion.identity);
+        yield return new WaitForSeconds(buildingToPlace.currentBuilding.buildTime);
+        Debug.Log("waited " + buildingToPlace.currentBuilding.buildTime + " seconds to build " + buildingToPlace.currentBuilding.name);
+        PlacementHelpers.ToggleRenderers(instance, true);
+        Destroy(go);
+    }
+
+
+    void FillRectWithTiles(Collider col)
+    {
+        if (activeTilesParent)
+            return;
+
+        Rect rect = PlacementHelpers.MakeRectOfCollider(col);
+        // Récupère la taille de l'objet à construire suivant l'axe X
+        float toX = col.gameObject.transform.localScale.x/2;
+        // Récupère la taille de l'objet à construire suivant l'axe X
+        float toZ = col.gameObject.transform.localScale.z/2;
+
+        GameObject parent = new GameObject("PlacementGrid");
+        parent.transform.SetParent(col.gameObject.transform.root);
+        parent.transform.position = col.gameObject.transform.InverseTransformPoint(new Vector3(0, 0.5f, 0));
+
+        // Création de la zone de construction (suivant les axes X et Z) avec l'affichage des collisions 
+        // pour montrer à l'utilisateur si la construction est possible ou non
+        for(float i = -toX; i < toX; i ++)
+        {
+            for(float j = -toZ; j < toZ; j ++)
+            {
+                GameObject tile = Instantiate(productionTile);
+                tile.transform.SetParent(parent.transform);
+                tile.transform.position = new Vector3(i, parent.transform.position.y, j);
+                activeTiles.Add(tile.GetComponent<ProductionTile>());
+            }
+        }
+        activeTilesParent = parent;
+    }
+
+
+    public void SpawnBuilding(BuildingSO building)
+    {
+        // Si l'objet n'a pas été placé alors return
+        if (currentSpawnedBuilding)
+            return;
+
+        currentSpawnedBuilding = Instantiate(building.buildingPrefab);
+        buildingToPlace.currentBuilding = building;
+        PlacementHelpers.ToggleRenderers(currentSpawnedBuilding, false);
+        Collider[] cols = currentSpawnedBuilding.GetComponentsInChildren<Collider>();
+        if(cols.Length > 0)
+            FillRectWithTiles(cols[0]);
+        else
+            Debug.LogError("Building has no colliders");
+    }
+}
